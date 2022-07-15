f83f6499af8a2f7b597487fd0c077fbd97075ada
jinhansol <109209446+jinhansol@users.noreply.github.com>
2022-07-15 18:23:24 +0900

    Update BoardDo.java

--- BoardDo.java
+++ BoardDo.java
@@ -9,4 +9,40 @@ public class BoardDo {
 	private String img ; 
 	private String wt ;
 	
+	public int getAid() {
+		return aid;
+	}
+	public void setAid(int aid) {
+		this.aid = aid;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public int getCount() {
+		return count;
+	}
+	public void setCount(int count) {
+		this.count = count;
+	}
+	public String getImg() {
+		return img;
+	}
+	public void setImg(String img) {
+		this.img = img;
+	}
 }
