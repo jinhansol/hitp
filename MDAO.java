cd07a36845d0af99cd8cdc7020d7c59625140bbe
jinhansol <109209446+jinhansol@users.noreply.github.com>
2022-07-15 16:18:57 +0900

    Update MDAO.java

--- MDAO.java
+++ MDAO.java
@@ -2,4 +2,18 @@ package tp;
 
 public class MDAO {
 
+  public void addBoard(BoardDo n) throws Exception {
+		Connection conn = open();
+		
+		String sql = "insert into news(title, img, date, content, count) values(?, ?, CURRENT_TIMESTAMPT(), ?)";
+		PreparedStatement pstmt = conn.prepareStatement(sql);
+		
+		try(conn; pstmt) {
+			pstmt.setString(1, n.getTitle());
+			pstmt.setString(2, n.getImg());
+			pstmt.setString(3,  n.getContent());
+			pstmt.executeUpdate();
+		}
+	}
+  
 }
